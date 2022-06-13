3235e8efdae334a2b5c527afe9c9b1ef9a6d7e62
Unknown Net <105541540+UnknownNet00000@users.noreply.github.com>
2022-06-13 03:50:04 +0300

    source

---
+++ source/calculationIndex.py
@@ -0,0 +1,19 @@
+# Yapacaginiz islemi secin
+print("Islem sec: \n\n")
+print("1. Toplama","\n2. Cikarma","\n3. Carpma","\n4. Bolme")
+
+# secim operatörü
+secmek = input("Seciminiz nedir? (1-2-3-4): ")
+s1 = int(input("1. Sayi: "))
+s2 = int(input("2. Sayi: "))
+
+if secmek == "1":
+    print(s1, "+", s2, "=",s1+s2)
+elif secmek == "2":
+    print(s1, "-", s2, "=",s1-s2)
+elif secmek == "3":
+    print(s1, "x", s2, "=",s1*s2)
+elif secmek == "4":
+    print(s1, "/", s2, "=",s1/s2)
+else:
+    print("Hatali secim yaptiniz. Lutfen dogru ornekte verildigi gibi dogru secim yazin.")
